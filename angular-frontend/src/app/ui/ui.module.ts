8d0a19c5a0906935b6dd15306bf873b21b1c8aff
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-16 17:40:35 -0600

    Added font-amesome to the anglular project, updated the header and footer to match

--- angular-frontend/src/app/ui/ui.module.ts
+++ angular-frontend/src/app/ui/ui.module.ts
@@ -4,13 +4,19 @@ import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import {AppRoutingModule} from "../app-routing.module";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fas, fab);
 
 @NgModule({
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent],
   imports: [
     AppRoutingModule,
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ]
 })
 export class UiModule { }
