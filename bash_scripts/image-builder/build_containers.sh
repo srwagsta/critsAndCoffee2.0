5baa294ab827a73c9a093e077891141587707d3d
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-24 09:10:02 -0600

    Cleaning up jenkins env cleanup step to allow failure while showing success in pipeline

--- bash_scripts/image-builder/build_containers.sh
+++ bash_scripts/image-builder/build_containers.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 cd ../../compose/
 
+docker-compose -f ./local.yml build pull
+
+docker-compose -f ./production.yml pull
+
 docker-compose -f ./local.yml build --force-rm --pull
 
 docker-compose -f ./production.yml build --force-rm --pull
--- bash_scripts/image-builder/build_test.sh
+++ bash_scripts/image-builder/build_test.sh
@@ -2,6 +2,8 @@
 
 cd ../../compose/
 
+docker-compose -f ./test.yml pull
+
 docker-compose -f ./test.yml build --force-rm --pull
 
 docker-compose -f ./test.yml push
