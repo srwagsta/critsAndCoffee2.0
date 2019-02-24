027345f34b22a20c558b26394f9b0dff7bdec8cd
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-24 07:39:07 -0600

    Fixing some permission problems with the Jenkins pipline

--- bash_scripts/image-builder/build_base_images.sh
+++ bash_scripts/image-builder/build_base_images.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+cd ../../compose/
 
-docker build -f ../../compose/base_images/Dockerfile.geo-django -t srwagsta/crits_and_coffee_accessories:geo-django ..
+docker build -f ./base_images/Dockerfile.geo-django -t srwagsta/crits_and_coffee_accessories:geo-django ..
 
 docker push srwagsta/crits_and_coffee_accessories:geo-django
