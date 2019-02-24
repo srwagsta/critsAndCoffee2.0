e109e2e48fb5d69af9ef610945e5870b2f395b07
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-23 20:08:05 -0600

    Fixed a naming error within the scripts

--- bash_scripts/image-builder/build_base_images.sh
+++ bash_scripts/image-builder/build_base_images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo "Starting geo django image build"
 
 docker build -f ../../compose/base_images/Dockerfile.geo-django -t srwagsta/crits_and_coffee_accessories:geo-django ..
 
