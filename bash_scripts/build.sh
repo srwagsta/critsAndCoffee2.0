12c51fb2d2b8967660d86e27ac5ed073f9a7b749
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-23 18:50:05 -0600

    Cleaning up the geoDjango builder

--- bash_scripts/build.sh
+++ bash_scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f ../compose/build_only/Dockerfile -t srwagsta/crits_and_coffee_accessories/geo-django:latest ..
+docker build -f ../compose/build_only/Dockerfile -t srwagsta/crits_and_coffee_accessories:geo-django ..
 
-docker push srwagsta/crits_and_coffee_accessories/geo-django:latest
+docker push srwagsta/crits_and_coffee_accessories:geo-django
