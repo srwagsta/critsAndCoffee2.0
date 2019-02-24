310d9f0f679e3f8647924f7dfc15cdea0150c98e
Stephen Wagstaff <srwagsta@gmail.com>
2019-02-24 09:20:51 -0600

    Cleaning up jenkins env cleanup step to allow failure while showing success in pipeline

--- bash_scripts/image-builder/build_base_images.sh
+++ bash_scripts/image-builder/build_base_images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cd ../../compose/
 
+docker pull srwagsta/crits_and_coffee_accessories:geo-django
+
 docker build -f ./base_images/Dockerfile.geo-django -t srwagsta/crits_and_coffee_accessories:geo-django ..
 
 docker push srwagsta/crits_and_coffee_accessories:geo-django
