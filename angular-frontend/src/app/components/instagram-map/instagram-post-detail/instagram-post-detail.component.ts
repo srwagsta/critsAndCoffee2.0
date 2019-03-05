7ada1ffba78fd13135537c4ed24c3cb03a653f59
Stephen Wagstaff <srwagsta@gmail.com>
2019-03-05 07:16:01 -0600

    Updated detail card for output emitter and 2-way data binding to parent

--- angular-frontend/src/app/components/instagram-map/instagram-post-detail/instagram-post-detail.component.ts
+++ angular-frontend/src/app/components/instagram-map/instagram-post-detail/instagram-post-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
 import {InstagramPostModel} from "../../../models/instagram-post.model";
 import {LoggingService} from "../../../services/logging.service";
 
@@ -13,9 +13,14 @@ export class InstagramPostDetailComponent implements OnInit {
 
   private _componentName: string = "Instagram Post Detail: ";
   @Input() post:InstagramPostModel;
+  @Output() postChange = new EventEmitter<InstagramPostModel>();
 
   ngOnInit() {
     this.log.info(`${this._componentName} Displaying post -- ${this.post.id_code}`);
+  }
+
+  public resetPost(){
+    this.postChange.emit(null);
   }
 
 }
