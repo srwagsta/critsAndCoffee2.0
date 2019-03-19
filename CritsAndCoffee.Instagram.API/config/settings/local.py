fe4de6fd6de023b3c00c0d2959c686c131ce93f8
Stephen Wagstaff <srwagsta@gmail.com>
2019-03-18 23:26:40 -0500

    Big refactor! Broke the client .NET core app away from the API and seperated the Docker/Compose structure to be able to better consolidate the entire app docker support in a single directory. The old front end still needs to be removed from the API and the new Client services need to be connected to the API via .NET core proxy services.

---
+++ CritsAndCoffee.Instagram.API/config/settings/local.py
@@ -0,0 +1,157 @@
+from .base import *  # noqa
+from .base import env
+from datetime import timedelta
+import environ
+
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env('DJANGO_SECRET_KEY', default='IrMUo8NRdNMboG7MyZF1wO39ElVgzvjgcHKqnrXqYRAnI5qTDxAjAV8JECTDGX6h')
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = [
+    "localhost",
+    "0.0.0.0",
+    "127.0.0.1",
+]
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': ''
+    }
+}
+
+# TEMPLATES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#templates
+TEMPLATES[0]['OPTIONS']['debug'] = DEBUG  # noqa F405
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = env('EMAIL_HOST', default='mailhog')
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = 1025
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ['debug_toolbar']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']  # noqa F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': [
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    ],
+    'SHOW_TEMPLATE_CONTEXT': True,
+}
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ['127.0.0.1', '10.0.2.2']
+if env('USE_DOCKER') == 'yes':
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [ip[:-1] + '1' for ip in ips]
+
+# django-extensions
+# ------------------------------------------------------------------------------
+# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
+INSTALLED_APPS += ['django_extensions']  # noqa F405
+# Celery
+# ------------------------------------------------------------------------------
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-always-eager
+CELERY_TASK_ALWAYS_EAGER = True
+# http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-eager-propagates
+CELERY_TASK_EAGER_PROPAGATES = True
+# AUTH
+# ------------------------------------------------------------------------------
+# https://django-allauth.readthedocs.io/en/latest/configuration.html
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+# http://getblimp.github.io/django-rest-framework-jwt/#additional-settings
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': timedelta(seconds=300),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=7),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+}
+
+# Logging
+# ------------------------------------------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['django_logfile'],
+    },
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(pathname)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'django_logfile': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/crits-logs/critsAndcoffee.log',
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['django_logfile'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['django_logfile'],
+            'propagate': False,
+        },
+        'django.security.DisallowedHost': {
+            'level': 'ERROR',
+            'handlers': ['django_logfile'],
+            'propagate': False,
+        },
+        'django': {
+            'level': 'DEBUG',
+            'handlers': ['django_logfile'],
+            'propagate': True,
+        },
+    },
+}
---
+++ Jenkinsfile
@@ -0,0 +1,56 @@
+// NOTE: the `push` directives only work because a `docker login` has been executed inside the build container
+pipeline {
+    agent none
+    stages {
+        stage('Add Private data to project for building from host') {
+            agent any
+            steps {
+                sh 'cp -r /var/jenkins_build_data/.private ./Docker/.envs'
+            }
+        }
+        stage('Build and Push new GeoDjango container') {
+            agent any
+            steps {
+                sh 'cd ./Docker/bash_scripts/image-builder && chmod 777 ./* && ./build_base_images.sh'
+            }
+        }
+        stage('Build and Push test images') {
+            agent any
+            steps {
+                sh 'cd ./Docker/bash_scripts/image-builder && chmod 777 ./* && ./build_test.sh'
+            }
+        }
+
+        stage('Run Tests') {
+            agent any
+            steps {
+                sh 'cd ./Docker/bash_scripts/Docker && chmod 777 ./* && ./execute_tests.sh'
+            }
+        }
+
+        stage('Build and Push local and production images') {
+            agent any
+            steps {
+                sh 'cd ./Docker/bash_scripts/image-builder && chmod 777 ./* && ./build_containers.sh'
+            }
+        }
+
+        stage('Deploy To Live') {
+            agent any
+            steps {
+                 //sh 'cd ./Docker/bash_scripts/Docker && chmod 777 ./* && ./live.sh'
+                 sh 'echo TODO: Deploy Live!'
+            }
+        }
+
+        stage('Cleanup Environment') {
+            agent any
+            steps {
+                sh 'cd ./Docker/bash_scripts/image-builder && chmod 777 ./* && ./cleanup_environment.sh'
+            }
+        }
+
+    }
+}
+
+
